552661b675227bf4e98ed08326c126560c46d35a
ulewu <jojo@ulewu.de>
2021-12-11 22:46:09 +0100

    Nacherfassen

--- frontend/src/components/Nacherfassen.vue
+++ frontend/src/components/Nacherfassen.vue
@@ -4,6 +4,28 @@
       Nacherfassen
     </p>   
         <button class="btn btn-info" v-on:click="TestBannerMessage()">Test-BannerMessage</button>
+
+        <div>
+          <table class="table table-dark">
+  <thead>
+    <tr>      
+      <th scope="col">Datum</th>
+      <th scope="col">Von</th>
+      <th scope="col">Bis</th>
+      <th scope="col">Was</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>   
+      <td><input type="date" id="entryDate" /></td>
+      <td><input type="time" id="entryFrom" /></td>
+      <td><input type="time" id="entrytimeTo" /></td>      
+      <td><input type="text" id="entryText" /></td>      
+    </tr>
+
+  </tbody>
+</table>
+        </div>
   </div>
 </template>
 
