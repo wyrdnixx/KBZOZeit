8f4aa2e2ef74c7c9f124bbb7ee497ba59c4c342d
ulewu <jojo@ulewu.de>
2021-12-13 23:01:27 +0100

    started Register

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -8,14 +8,18 @@
         <br>
         Bitte registrieren
         <hr>
-        Wer sind Sie? <input type="text" placeholder="Name" v-model="UserName"/>
+        Wer sind Sie? <input type="text" placeholder="Name" v-model="Ident.Name"/>
         <hr>
-        Debug - Name: {{this.UserName}}
+        Debug -> Name: {{this.Ident.Name}} UUID: {{this.Ident.UUID}} Status: {{this.Ident.Status}}
+        <button class="btn btn-secondary" v-on:click="RegisterIdent()">Register</button>
     </div>
   </div>
 </template>
 
 <script>
+import { uuid } from 'vue-uuid'; 
+import axios from 'axios';
+const apiURL = window.location.protocol + "//"+ window.location.hostname +":8081/api"
 
 export default {
   name: 'Register', 
@@ -27,9 +31,39 @@ export default {
   },
   data() {
       return {
-          UserName: ""
+          Ident: {
+            Name: "",
+            UUID: "",
+            Status:""
+          } 
       }
-  }
+  },
+  created() {
+
+  },
+  methods: {
+
+    testMethod() {
+      this.Ident.UUID = uuid.v4()
+      this.Ident.Status=""       
+    },
+    async RegisterIdent() {
+          console.log("RegisterIdent")
+          if (this.Ident.Name === "") {
+            this.$parent.showAlert("Kein Username angegeben!")
+          } else {
+                  
+            await   axios.post(apiURL + "/RegisterIdent", {           
+              Ident: this.Ident
+            }, {
+              headers: {
+                'Content-Type': 'application/json',
+              }
+            }).catch((error)=> this.$parent.showAlert("Server returned an Error:\n" + error));  
+    
+          }
+    },        
+  }       
 }
 </script>
 
